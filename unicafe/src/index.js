a98914b35145e97174c0bb6bc8383dbca88a0fb9
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2019-08-09 11:41:55 +0300

    unicafe step 6

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -15,52 +15,82 @@ const App = () => {
         <div>
             <h1>give feedback</h1>
 
-            <button onClick={() => setGood(good + 1)}>good</button>
-            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-            <button onClick={() => setBad(bad + 1)}>bad</button>
+            <Button click={() => setGood(good + 1)} text='good' />
+            <Button click={() => setNeutral(neutral + 1)} text='neutral' />
+            <Button click={() => setBad(bad + 1)} text='bad' />
 
             <h1>statistics</h1>
 
-            <Stats good={good} neutral={neutral} bad={bad} />
+            <Statistics states={{ good, neutral, bad }} />
+
         </div>
     )
 }
 
-// const Button = (props) => {
-//     const { text, clickFunc } = props
+const Button = (props) => {
+
+
+
+    return (
+        <button onClick={props.click}>{props.text}</button>
+    )
+}
+
+// Used in step 5
+// const Statistic = (props) => {
+
 //     return (
-//         <button onClick={clickFunc}>text</button>
+//         <p>{props.text} {props.value}</p>
 //     )
 // }
 
-const Stats = (props) => {
-    const { good, neutral, bad } = props
-    let sum = good + neutral + bad
-    let avg = (good + 0 * neutral + -1 * bad) / sum;
-    // Define average here! Current is messed up
+const Statistics = (props) => {
 
-    let positive = 100 * good / sum
+    // This is not the way to do this, but I'm going to do it anyway
+    const { good, neutral, bad } = props.states
+
+
+    let sum = good + neutral + bad
+    let avg = (good - bad) / sum
+    let pos = 100 * good / sum
+
     if (sum === 0) {
         return (
-            <p>No feedback  given</p>
+            <p>No feedback given</p>
         )
     }
+
     return (
 
-        <div>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {sum}</p>
 
-            <p>average {avg}</p>
-            <p>positive {positive}%</p>
-        </div>
+        //unicafe step 5
+        // <div>
+
+        //     <Statistic text='good' value={good} />
+        //     <Statistic text='neutral' value={neutral} />
+        //     <Statistic text='bad' value={bad} />
+        //     <Statistic text='all' value={sum} />
+        //     <Statistic text='average' value={avg} />
+        //     <Statistic text='positive' value={pos} />
+        // </div>
+
+        //unicafe step 6
+        <table>
+            <tbody>
+
+                <tr><td>good</td><td>{good}</td></tr>
+                <tr><td>neutral</td><td>{neutral}</td></tr>
+                <tr><td>bad</td><td>{bad}</td></tr>
+                <tr><td>all</td><td>{sum}</td></tr>
+                <tr><td>average</td><td>{avg}</td></tr>
+                <tr><td>positive</td><td>{pos}</td></tr>
+
+            </tbody>
+
+        </table>
     )
+
 }
-
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
